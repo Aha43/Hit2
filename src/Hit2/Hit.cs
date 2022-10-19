043efe9380f3973a20ed4628d9c545e470fa949d
Arne Halvorsen <arne.halvorsen@gmail.com>
2022-10-19 07:23:59 +0200

    wip: can build tree of test nodes, added console prj (for now for experimenting, later may become cli)

---
+++ src/Hit2/Hit.cs
@@ -0,0 +1,32 @@
+﻿namespace Hit2
+{
+    public class Hit
+    {
+        private readonly List<TestNode> _testNodes = new ();
+
+        public TestNode Do(string testName)
+        {
+            if (_testNodes.Any(e => e.TestName.Equals(testName)))
+            {
+                throw new ArgumentException($"duplicated named test node: {testName}");
+            }
+
+            var retVal = new TestNode(testName);
+            _testNodes.Add(retVal);
+            return retVal;
+        }
+
+        public override string ToString()
+        {
+            var pv = new PrintTestNodeVisitor();
+            foreach (var node in _testNodes)
+            {
+                pv.Visit(node);
+            }
+
+            return pv.ToString();
+        }
+
+    }
+    
+}
---
+++ src/Hit2Cli/Program.cs
@@ -0,0 +1,11 @@
+﻿using Hit2;
+
+Console.WriteLine("Hello, World!");
+
+var hit = new Hit();
+
+hit.Do("CreatePerson")
+    .Do("UpdatePerson")
+        .Do("DeletePerson");
+
+Console.WriteLine(hit.ToString());
